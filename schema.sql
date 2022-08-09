91e7d205ab0eaf8228b082714c3bd1abfacacaf2
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-08-09 15:22:28 +0100

    Add data to database and the parameters

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    data_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10,2),
 );
+
+
