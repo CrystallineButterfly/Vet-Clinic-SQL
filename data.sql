9ef33ba9c77e49f9ea824ce727f82c4f650eb6a5
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-08-10 14:10:01 +0100

    Add SELECT queries for data in coloums in table

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
-/* Populate database with sample data. */
+/* Populate database with vetinary data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO animal (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.0),
 (3, 'Pikachu', '2021-07-01', 1, FALSE, 15.04),
 (4, 'Devimon', '2017-05-17', 5, TRUE, 11),
-(5, 'Agumon', '2020-02-03', 0. TRUE, 10.23);
+(5, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
