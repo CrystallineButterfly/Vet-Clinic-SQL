9d380c9bb31ffef27b6220f5ac836cb477472010
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-08-11 09:41:20 +0100

    Add more queries and tranacts

--- queries.sql
+++ queries.sql
@@ -8,3 +8,34 @@ SELECT name, escape_attempts from animal WHERE weight_kg > 10.5;
 SELECT * from animal WHERE neutered = 't';
 SELECT * from animal WHERE name <> 'Gabumon';
 SELECT * from animal WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+UPDATE animal
+SET species = 'unspecified';
+SELECT * from animal;
+ROLLBACK;
+SELECT * from animal;
+
+BEGIN;
+UPDATE animal SET species = 'digimon'
+WHERE name like '%mon';
+UPDATE animal SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animal
+WHERE data_of_birth > 'January 1, 2022';
+SAVEPOINT SP1;
+UPDATE animal SET weight_kg = (weight_kg * -1);
+ROLLBACK TO SP1;
+UPDATE animal SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animal;
+SELECT * from animal WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animal;
+SELECT neutered, MAX(escape_attempts) FROM animal GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animal GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animal WHERE data_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
